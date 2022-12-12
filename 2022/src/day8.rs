427c8531b54fa6dea67e027bccd9528ce9cc1b28
Corey Porter <corey.porter@getcruise.com>
2022-12-12 19:55:27 +0000

    day8

--- 2022/src/day8.rs
+++ 2022/src/day8.rs
@@ -1,5 +1,5 @@
 use std::io::BufRead;
-
+use std::cmp::max;
 
 #[derive(Debug)]
 struct Tree {
@@ -8,6 +8,45 @@ struct Tree {
 }
 
 type Trees = Vec<Vec<Tree>>;
+
+
+
+fn scenic_score(t: &Trees, c: usize, r: usize) -> usize {
+    let max_cols = t[0].len();
+    let max_rows = t.len();
+    let h = t[r][c].height;
+
+    let mut lc: usize = 0;
+    let mut rc: usize = 0;
+    let mut uc: usize = 0;
+    let mut dc: usize = 0;
+
+    for cc in (0..c).rev() {
+        lc += 1;
+        if t[r][cc].height >= h {
+            break;
+        }
+    }
+    for cc in (1 + c)..max_cols {
+        rc += 1;
+        if t[r][cc].height >= h {
+            break;
+        }
+    }
+    for rr in (0..r).rev() {
+        uc += 1;
+        if t[rr][c].height >= h {
+            break;
+        }
+    }
+    for rr in (1 + r)..max_rows {
+        dc += 1;
+        if t[rr][c].height >= h {
+            break;
+        }
+    }
+    lc * rc * uc * dc
+}
 
 fn main() {
     let mut trees: Trees = std::io::stdin()
@@ -72,5 +111,13 @@ fn main() {
         .map(|row| row.iter().filter(|t| t.visible).count())
         .sum();
 
+    let mut day2 = 0;
+    for r in 0..rows {
+        for c in 0..cols {
+            day2 = max(day2, scenic_score(&trees, r, c));
+        }
+    }
+
     println!("day1: {}", day1);
+    println!("day2: {}", day2);
 }
