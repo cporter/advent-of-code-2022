bf1cd0d77d7dd7c14eda6be83d5644132ffebbef
Corey Porter <corey.porter@getcruise.com>
2022-12-22 15:08:15 +0100

    day 11 part 2

--- 2022/src/day11.rs
+++ 2022/src/day11.rs
@@ -7,38 +7,48 @@ use std::str::FromStr;
 #[derive(Debug)]
 enum Value {
     Old,
-    Num(i32)
+    Num(i64),
 }
 
 #[derive(Debug)]
 enum Op {
     Add,
     Sub,
-    Mul
+    Mul,
 }
-
 
 #[derive(Debug)]
 struct Monkey {
-    items : Vec<i32>,
-    op : Op,
-    op_a : Value,
-    op_b : Value,
-    test : i32,
-    true_throw : usize,
-    false_throw : usize,
+    items: Vec<i64>,
+    op: Op,
+    op_a: Value,
+    op_b: Value,
+    test: i64,
+    true_throw: usize,
+    false_throw: usize,
     counted: usize,
 }
 
-fn inspect(m : &Monkey, x : i32) -> i32 {
-    let l = match m.op_a { Value::Old => x, Value::Num(n) => n};
-    let r = match m.op_b { Value::Old => x, Value::Num(n) => n};
-    let pre = match m.op {
+fn inspect(m: &Monkey, x: i64) -> i64 {
+    let l = match m.op_a {
+        Value::Old => x,
+        Value::Num(n) => n,
+    };
+    let r = match m.op_b {
+        Value::Old => x,
+        Value::Num(n) => n,
+    };
+    match m.op {
         Op::Add => l + r,
         Op::Sub => l - r,
-        Op::Mul => l * r
-    };
-    pre / 3
+        // Op::Mul => l * r,
+        Op::Mul => {
+            match l.checked_mul(r) {
+                Some(x) => x,
+                None => panic!("still trying to multiply {l} and {r}"),
+            }
+        }
+    }
 }
 
 impl Default for Monkey {
@@ -64,8 +74,8 @@ impl FromStr for Monkey {
             static ref OPERATION: Regex = Regex::new(r"new = ([^\s]+) ([-+*]) ([^\s]+)").unwrap();
             static ref TEST: Regex = Regex::new(r"divisible by (\d+)").unwrap();
             static ref MONKEY: Regex = Regex::new(r"monkey (\d+)").unwrap();
-        }        
-        let lines : Vec<&str> = lines_raw.split("\n").collect();
+        }
+        let lines: Vec<&str> = lines_raw.split("\n").collect();
         let mut ret = Monkey::default();
         {
             let cap = ITEMS.captures_iter(lines[1]).next().unwrap();
@@ -77,7 +87,7 @@ impl FromStr for Monkey {
                 "+" => Op::Add,
                 "*" => Op::Mul,
                 "-" => Op::Sub,
-                _ => panic!("Unexpected operator: {}", &cap[2])
+                _ => panic!("Unexpected operator: {}", &cap[2]),
             };
             ret.op_a = match &cap[1] {
                 "old" => Value::Old,
@@ -105,33 +115,42 @@ impl FromStr for Monkey {
     }
 }
 
-fn main() {
-    let stdin = std::io::read_to_string(std::io::stdin()).unwrap();
-    let mut monkeys : Vec<Monkey> = stdin.split("\n\n").map(|s| s.parse().unwrap()).collect();
+fn solve(n: usize, divisor: i64, monkeys: &mut Vec<Monkey>) -> usize {
+    let common_divisor: i64 = monkeys.iter().map(|m| m.test).product();
+    (0..n).for_each(|_| for i in 0..monkeys.len() {
+        monkeys[i].counted += monkeys[i].items.len();
+        let items: Vec<i64> = monkeys[i].items.to_vec();
+        monkeys[i].items.clear();
 
-    (0..20).for_each(|_| {
-        for i in 0..monkeys.len() {    
-            monkeys[i].counted += monkeys[i].items.len();
-            let items : Vec<i32> = monkeys[i].items.to_vec();
-            monkeys[i].items.clear();
-
-            items.iter().for_each(|item| {
-                let newval = inspect(&monkeys[i], *item);
-                let target = if 0 == newval % monkeys[i].test {
-                    monkeys[i].true_throw
-                } else {
-                    monkeys[i].false_throw
-                };
-                monkeys[target].items.push(newval);
-            });
-        }
+        items.iter().for_each(|item| {
+            let val = *item % common_divisor;
+            let newval = inspect(&monkeys[i], val) / divisor;
+            let target = if 0 == newval % monkeys[i].test {
+                monkeys[i].true_throw
+            } else {
+                monkeys[i].false_throw
+            };
+            monkeys[target].items.push(newval);
+        });
     });
 
-    let mut counts : Vec<usize> = monkeys.iter().map(|m| m.counted).collect();
+    let mut counts: Vec<usize> = monkeys.iter().map(|m| m.counted).collect();
     counts.sort();
 
-    let s = &counts[counts.len()-2..counts.len()];
-    let part1 = s.iter().map(|x| *x).reduce(|a,b| a * b).unwrap();
+    let s = &counts[counts.len() - 2..counts.len()];
+    s.iter().map(|x| *x).product()
+}
+
+fn main() {
+    let stdin = std::io::read_to_string(std::io::stdin()).unwrap();
+    let mut monkeys: Vec<Monkey> = stdin.split("\n\n").map(|s| s.parse().unwrap()).collect();
+
+    let part1 = solve(20, 3, &mut monkeys);
+
+    monkeys = stdin.split("\n\n").map(|s| s.parse().unwrap()).collect();
+
+    let part2 = solve(10000, 1, &mut monkeys);
 
     println!("part 1: {part1}");
+    println!("part 2: {part2}");
 }
